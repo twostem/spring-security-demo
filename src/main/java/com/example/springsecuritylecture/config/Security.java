4bbe6f7389f8d0035e3b05f53be4992657b95e69
root <root@P94101657-001.CNS.LGCNS.COM>
2024-06-05 17:38:45 +0900

    remember-me config

--- src/main/java/com/example/springsecuritylecture/config/Security.java
+++ src/main/java/com/example/springsecuritylecture/config/Security.java
@@ -1,9 +1,11 @@
 package com.example.springsecuritylecture.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -15,6 +17,8 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.Arrays;
 
+import static org.springframework.security.config.Customizer.*;
+
 @Configuration
 @EnableWebSecurity
 public class Security {
@@ -24,23 +28,23 @@ public class Security {
     public SecurityFilterChain config(HttpSecurity http) throws Exception {
 
         http
-//                .httpBasic(withDefaults())
-                .httpBasic(basic ->
-                        basic
-                                // 인증 실패시 실행
-                                // BasicAuthenticationEntryPoint 참고
-                                .authenticationEntryPoint(authenticationEntryPoint)
-                )
+                .formLogin(withDefaults())
                 .authorizeHttpRequests(auth -> {
                     auth
                             .anyRequest()
                             .authenticated();
-                });
+                })
+                .rememberMe(remember ->
+                        remember
+                                .alwaysRemember(false)
+                                .tokenValiditySeconds(3600)
+                                .userDetailsService(inmemoryDetailsService())
+                );
 
         return http.build();
     }
 
-    @Bean
+    @Bean(name = "inmemory")
     public UserDetailsService inmemoryDetailsService() {
         UserDetails user1 = User.withUsername("user1").password("{noop}1234").roles("USER").build();
         UserDetails user2 = User.withUsername("user2").password("{noop}1234").roles("USER").build();
