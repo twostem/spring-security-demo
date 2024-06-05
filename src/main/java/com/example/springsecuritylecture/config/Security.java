cf14ec1937377561d8aa04691688046172e7281a
root <root@P94101657-001.CNS.LGCNS.COM>
2024-06-05 16:18:59 +0900

    configuration

--- src/main/java/com/example/springsecuritylecture/config/Security.java
+++ src/main/java/com/example/springsecuritylecture/config/Security.java
@@ -2,48 +2,40 @@ package com.example.springsecuritylecture.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.Arrays;
 
-import static org.springframework.security.config.Customizer.*;
-
 @Configuration
 @EnableWebSecurity
 public class Security {
+    private final String realmBasic = "realm basic";
 
     @Bean
     public SecurityFilterChain config(HttpSecurity http) throws Exception {
+
         http
-                .authorizeHttpRequests(auth ->
-                        auth
-                                .anyRequest()
-                                .authenticated()
+//                .httpBasic(withDefaults())
+                .httpBasic(basic ->
+                        basic
+                                // 인증 실패시 실행
+                                // BasicAuthenticationEntryPoint 참고
+                                .authenticationEntryPoint(authenticationEntryPoint)
                 )
-                .formLogin(form ->
-                        form
-                                .loginProcessingUrl("/loginProc")
-                                .defaultSuccessUrl("/")
-                                .failureUrl("/failed")
-                                .usernameParameter("userId")
-                                .passwordParameter("password")
-                                .successHandler(((request, response, authentication) -> {
-                                    System.out.println("authentication: " + authentication);
-                                    response.sendRedirect("/home");
-                                }))
-                                .failureHandler(((request, response, exception) -> {
-                                    System.out.println("exception: " + exception.getMessage());
-                                    response.sendRedirect("/login");
-                                }))
-                                .permitAll()
-                );
+                .authorizeHttpRequests(auth -> {
+                    auth
+                            .anyRequest()
+                            .authenticated();
+                });
 
         return http.build();
     }
@@ -56,4 +48,10 @@ public class Security {
 
         return new InMemoryUserDetailsManager(Arrays.asList(user1, user2, user3));
     }
+
+    // BasicAuthenticationEntryPoiont 와 동일
+    AuthenticationEntryPoint authenticationEntryPoint = (request, response, authException) -> {
+        response.setHeader(HttpHeaders.WWW_AUTHENTICATE.toString(), "Basic realm=\"" + realmBasic + "\"");
+        response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
+    };
 }
