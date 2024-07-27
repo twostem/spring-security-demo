3ba92a590b504458a20cef0152f8e7928eac493e
twostem <twostem@github.com>
2024-07-27 19:32:35 +0900

    feat: RequestCache/SavedRequest

--- src/main/java/com/example/springsecuritylecture/config/Security.java
+++ src/main/java/com/example/springsecuritylecture/config/Security.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.savedrequest.CookieRequestCache;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.Arrays;
@@ -22,8 +23,17 @@ import static org.springframework.security.config.Customizer.*;
 @EnableWebSecurity
 public class Security {
 
+    // RequestCache: 인증 절차 문제로 리다이렉트 된 후에 이전에 했던 요청 정보를 담고 있는 'SavedRequest' 객체를 쿠키 혹은 세션에 저장하고
+    // 필요시 다시 가져와 실행하는 캐시 메카니즘
+
+    // SavedRequest: 로그인과 같은 인증 절차 후 사용자를 인증 이전의 원래 페이지로 안내하며 이전 요청과 관련된 여려 정보를 저장한다.
+
     @Bean
     public SecurityFilterChain config(HttpSecurity http) throws Exception {
+        CookieRequestCache cookieRequestCache = new CookieRequestCache();
+        new RequestMatcher
+        cookieRequestCache.setRequestMatcher(Req);
+
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/logoutSuccess").permitAll()
@@ -31,26 +41,8 @@ public class Security {
                         .authenticated()
                 )
                 .formLogin(Customizer.withDefaults())
-                .logout(logout -> logout
-                        .logoutUrl("/logoutProc")
-                        // csrf를 disable 하거나 RequestMatcher를 사용하면 logout 실행을 POST 이외의 방법으로 할 수 있음
-                        .logoutRequestMatcher(
-                                new AntPathRequestMatcher("/logoutProc", "GET")
-                        )
-                        .logoutSuccessUrl("/logoutSuccess")
-                        .logoutSuccessHandler((request, response, authentication) -> {
-                            response.sendRedirect("/logoutSuccess");
-                        })
-                        .deleteCookies("JSESSIONID", "remember-me")
-                        .invalidateHttpSession(true)
-                        .clearAuthentication(true)
-                        .addLogoutHandler(((request, response, authentication) -> {
-                            HttpSession session = request.getSession();
-                            session.invalidate();
-                            SecurityContextHolder.getContextHolderStrategy().getContext().setAuthentication(null);
-                            SecurityContextHolder.getContextHolderStrategy().clearContext();
-                        }))
-                        .permitAll()
+                .requestCache(requestCache -> requestCache
+                        .requestCache(new CookieRequestCache())
                 )
 
         ;
