946889a9ed6ddee281000a75cd32a83b122430a7
twostem <twostem@github.com>
2024-07-27 18:48:15 +0900

    feat: logout

--- src/main/java/com/example/springsecuritylecture/config/Security.java
+++ src/main/java/com/example/springsecuritylecture/config/Security.java
@@ -1,15 +1,18 @@
 package com.example.springsecuritylecture.config;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.Arrays;
 
@@ -23,18 +26,33 @@ public class Security {
     public SecurityFilterChain config(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/anonymous").hasRole("GUEST")
-                        .requestMatchers("/authenticationContext", "/authentication").permitAll()
+                        .requestMatchers("/logoutSuccess").permitAll()
                         .anyRequest()
                         .authenticated()
                 )
                 .formLogin(Customizer.withDefaults())
-                // anonymousAuthenticationFilter까지 왔을때도 securityContext에 authentication 이 null이면
-                // anonymousAthentication token 부여
-                .anonymous(anonymous -> anonymous
-                        .principal("guest")
-                        .authorities("ROLE_GUEST")
+                .logout(logout -> logout
+                        .logoutUrl("/logoutProc")
+                        // csrf를 disable 하거나 RequestMatcher를 사용하면 logout 실행을 POST 이외의 방법으로 할 수 있음
+                        .logoutRequestMatcher(
+                                new AntPathRequestMatcher("/logoutProc", "GET")
+                        )
+                        .logoutSuccessUrl("/logoutSuccess")
+                        .logoutSuccessHandler((request, response, authentication) -> {
+                            response.sendRedirect("/logoutSuccess");
+                        })
+                        .deleteCookies("JSESSIONID", "remember-me")
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .addLogoutHandler(((request, response, authentication) -> {
+                            HttpSession session = request.getSession();
+                            session.invalidate();
+                            SecurityContextHolder.getContextHolderStrategy().getContext().setAuthentication(null);
+                            SecurityContextHolder.getContextHolderStrategy().clearContext();
+                        }))
+                        .permitAll()
                 )
+
         ;
 
         return http.build();
